b8cc99c81b382bf9ceb97c6d8848f638f4f2e602
kavinvin <kavinvin.vin@gmail.com>
2020-07-23 15:17:37 +0000

    Add conda hook

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,5 +27,5 @@ load
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-#eval /home/kavinvin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /home/kavinvin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
